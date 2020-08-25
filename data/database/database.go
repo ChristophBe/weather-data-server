4f02f9ae1ebb82b0a8eb66bcff01df704b6e48c9
Christoph Becker <post@christophb.de>
2020-08-25 17:19:46 +0200

    update neo4j driver version

--- data/database/database.go
+++ data/database/database.go
@@ -14,15 +14,20 @@ func createDriver() (driver neo4j.Driver, err error) {
 		return
 	}
 
+
+	configForNeo4j40 := func(conf *neo4j.Config) { conf.Encrypted = false }
 	databaseHost := fmt.Sprintf("%s:%d", configuration.Neo4j.Host, configuration.Neo4j.Port)
-	driver, err = neo4j.NewDriver(databaseHost, neo4j.BasicAuth(configuration.Neo4j.Username, configuration.Neo4j.Password, ""))
+	driver, err = neo4j.NewDriver(databaseHost, neo4j.BasicAuth(configuration.Neo4j.Username, configuration.Neo4j.Password, ""),configForNeo4j40)
+	if err != nil {
+		err = errors.Errorf("can not create neo4j driver; cause: %w",err)
+	}
 	return
 
 }
 func createSession(driver neo4j.Driver, mode neo4j.AccessMode) (neo4j.Session, error) {
 	session, err := driver.Session(mode)
 	if err != nil {
-		return nil, err
+		return nil, errors.Errorf("can not start neo4j session; cause: %w",err)
 	}
 
 	return session, err
@@ -37,21 +42,23 @@ func doReadTransaction(statement string, params map[string]interface{}, resultHa
 	)
 
 	driver, err = createDriver()
-	if err != nil {
-		return nil, err
-	}
 	defer driver.Close()
+	if err != nil {
+		return nil, errors.Errorf("can not do read transaction; cause: %w",err)
+	}
+
 
 	session, err = createSession(driver, neo4j.AccessModeRead)
-	if err != nil {
-		return nil, err
-	}
 	defer session.Close()
+
+	if err != nil {
+		 return nil, errors.Errorf("can not do read transaction; cause: %w",err)
+	}
 
 	return session.ReadTransaction(func(transaction neo4j.Transaction) (res interface{}, err error) {
 		result, err = transaction.Run(statement, params)
 		if err != nil {
-			return nil, err
+			return nil,  errors.Errorf("can not run read transaction; cause: %w",err)
 		}
 		return resultHandler(result)
 	})
@@ -66,21 +73,23 @@ func doWriteTransaction(statement string, params map[string]interface{}, resultH
 	)
 
 	driver, err = createDriver()
-	if err != nil {
-		return nil, err
-	}
 	defer driver.Close()
+	if err != nil {
+		return nil, errors.Errorf("can not create driver; cause: %w",err)
+	}
+
 
 	session, err = createSession(driver, neo4j.AccessModeWrite)
-	if err != nil {
-		return nil, err
-	}
 	defer session.Close()
+	if err != nil {
+		return nil, errors.Errorf("can not create session; cause: %w",err)
+	}
+
 
 	return session.WriteTransaction(func(transaction neo4j.Transaction) (res interface{}, err error) {
 		result, err = transaction.Run(statement, params)
 		if err != nil {
-			return nil, err
+			return nil, errors.Errorf("can not run write transaction; cause: %w",err)
 		}
 		return resultHandler(result)
 	})
@@ -98,12 +107,15 @@ func parseSingleItemFromResult(parseSingeRecord func(record neo4j.Record) (res i
 	return func(result neo4j.Result) (res interface{}, err error) {
 		if result.Next() {
 			res, err = parseSingeRecord(result.Record())
+			if err != nil {
+				err=  errors.Errorf("can not run parse result; cause: %w",err)
+			}
 			return
 		}
 		err = result.Err()
 
 		if err == nil {
-			err = errors.New("node not found")
+			err = errors.Errorf("node not found; cause: %w", err)
 		}
 		return
 	}
@@ -117,7 +129,7 @@ func parseListFromResult(parseSingeRecord func(record neo4j.Record) (res interfa
 			item, err := parseSingeRecord(result.Record())
 
 			if err != nil {
-				return nil, err
+				return nil,  errors.Errorf("can not run parse list form record; cause: %w",err)
 			}
 			list = append(list, item)
 		}
--- go.mod
+++ go.mod
@@ -4,13 +4,8 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/golang/mock v1.3.1 // indirect
 	github.com/gorilla/mux v1.7.3
-	github.com/neo4j-drivers/gobolt v1.7.4 // indirect
-	github.com/neo4j/neo4j-go-driver v1.7.4
-	github.com/onsi/ginkgo v1.10.3 // indirect
-	github.com/onsi/gomega v1.7.1 // indirect
-	github.com/pkg/errors v0.8.1
-	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/neo4j/neo4j-go-driver v1.8.1
+	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413
 )
